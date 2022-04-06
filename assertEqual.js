e3826400ace7d7cddf04bac21a568df0a3e763a7
Aaron Au <aaron.au@me.com>
2022-04-06 01:58:34 +0000

    Fixed assertion failed string

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🔴 Assertion Failed: ${actual} !=== ${expected}`);
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
