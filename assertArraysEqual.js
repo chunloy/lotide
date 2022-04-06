fe579c4e83faa7a36c69591c8a256805a91c63f5
Aaron Au <aaron.au@me.com>
2022-04-06 02:00:02 +0000

    initial commit. Checks if arrays are the same and prints a message

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  let lengthMatch = true;
+  let elementMatch = true;
+
+  //check if lengths match
+  if (arrayOne.length !== arrayTwo.length) {
+    lengthMatch = false;
+  }
+  //check if elements match
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      elementMatch = false;
+    }
+  }
+  console.log((lengthMatch && elementMatch ? 
+    `🟢 Assertion Passed: [${arrayOne}] === [${arrayTwo}]` : `🔴 Assertion Failed: [${arrayOne}] !== [${arrayTwo}]`));
+};
+//should print assertion passed
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+//should print assertion failed
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
