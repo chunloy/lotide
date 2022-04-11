2674e44f5c0af5f7337477f5d37a2732b9c311fb
Aaron Au <aaron.au@me.com>
2022-04-11 17:14:12 +0000

    Refactor code by moving test code to separate file.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,30 @@
+//import module
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   console.log((eqArrays(arrayOne, arrayTwo) ?
     `🟢 Assertion Passed: [${arrayOne}] === [${arrayTwo}]` : `🔴 Assertion Failed: [${arrayOne}] !== [${arrayTwo}]`));
 };
 
-const eqArrays = function(arrayOne, arrayTwo) {
-  //check if lengths match
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  //check if elements match
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-//should print assertion passed
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// const eqArrays = function(arrayOne, arrayTwo) {
+//   //check if lengths match
+//   if (arrayOne.length !== arrayTwo.length) {
+//     return false;
+//   }
+//   //check if elements match
+//   for (let i = 0; i < arrayOne.length; i++) {
+//     if (arrayOne[i] !== arrayTwo[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
-//should print assertion failed
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+// //should print assertion passed
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+// //should print assertion failed
+// assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+
+//export module
+module.exports = assertArraysEqual;
