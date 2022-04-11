c55d6963fe8a0c14e605523a563f47080ce52d8e
Aaron Au <aaron.au@me.com>
2022-04-11 22:41:08 +0000

    Remove commented code from file.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,25 +6,5 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
     `🟢 Assertion Passed: [${arrayOne}] === [${arrayTwo}]` : `🔴 Assertion Failed: [${arrayOne}] !== [${arrayTwo}]`));
 };
 
-// const eqArrays = function(arrayOne, arrayTwo) {
-//   //check if lengths match
-//   if (arrayOne.length !== arrayTwo.length) {
-//     return false;
-//   }
-//   //check if elements match
-//   for (let i = 0; i < arrayOne.length; i++) {
-//     if (arrayOne[i] !== arrayTwo[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// //should print assertion passed
-// assertArraysEqual([1, 2, 3], [1, 2, 3]);
-
-// //should print assertion failed
-// assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
-
 //export module
 module.exports = assertArraysEqual;
