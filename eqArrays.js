a1da594e47aebb779067e38fa67424a0a6691be3
Aaron Au <aaron.au@me.com>
2022-04-11 16:57:28 +0000

    Refactor code by moving test code to separate file.

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !=== ${expected}`);
-  }
-};
-
 const eqArrays = function(arrayOne, arrayTwo) {
   //check if lengths match
   if (arrayOne.length !== arrayTwo.length) {
@@ -20,22 +12,5 @@ const eqArrays = function(arrayOne, arrayTwo) {
   return true;
 };
 
-//test case: arrays identical
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //assertion passed
-assertEqual(eqArrays([1, 'hi', 'hello'], [1, 'hi', 'hello']), true); //assertion passed
-assertEqual(eqArrays([1, [1, 2, 3]], [1, [1, 2, 3]]), true); //assertion failed, ok for now
-assertEqual(eqArrays([[1, 2], { day: 5, month: 'April' }], [[1, 2], { day: 5, month: 'April' }]), true); //assertion failed, ok for now
-
-//test case: elements don't match
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); //assertion passed
-assertEqual(eqArrays([1, 'hi', 'bye'], [1, 'bye', 'hi']), false); //assertion passed
-
-//test case: lengths don't match
-assertEqual(eqArrays([1, 2, 3], [4, 3, 2, 1]), false); //assertion passed
-
-//test case: one array empty
-assertEqual(eqArrays([], [1, 2, 3]), false); //assertion passed
-
-//test case: two empty arrays
-assertEqual(eqArrays([], []), true); //assertion passed
-
+//export module
+module.exports = eqArrays;
