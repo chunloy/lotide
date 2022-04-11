b806adb85582b15c3ca3800700957079259006ce
Aaron Au <aaron.au@me.com>
2022-04-11 22:47:21 +0000

    Refactor code by moving test code to separate file.

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-//TEST FUNCTION
-const eqArrays = function(arrayOne, arrayTwo) {
-  //check if lengths match
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  //check if elements match
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-//TEST FUNCTION
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  console.log((eqArrays(arrayOne, arrayTwo) ?
-    `🟢 Assertion Passed: [${arrayOne}] === [${arrayTwo}]` : `🔴 Assertion Failed: [${arrayOne}] !== [${arrayTwo}]`));
-};
-
-//ACTUAL CODE
 const middle = function(array) {
   let middleArray = [];
   if (array.length <= 2) {
@@ -32,14 +10,5 @@ const middle = function(array) {
   return middleArray;
 };
 
-//test case: array length <= 2
-assertArraysEqual(middle([1]), []); //passed, OK
-assertArraysEqual(middle([1, 2]), []); //passed, OK
-
-//test case: odd number arrays
-assertArraysEqual(middle([1, 2, 3]), [2]); //passed, OK
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); //passed, OK
-
-//test case: even number arrays
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); //passed, OK
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]); //passed, OK
+//export module
+module.exports = middle;
