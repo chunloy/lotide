dc5aafdb0243f7157a664501f2f0ddb553e97f97
Aaron Au <aaron.au@me.com>
2022-04-12 05:14:57 +0000

    Initial commit. Set up test code using M & C. Confirmed that test runs as expected.

---
+++ test/countOnlyTest.js
@@ -0,0 +1,14 @@
+//import module
+const { countOnly } = require('../index');
+const assert = require('chai').assert;
+
+describe("#countOnly", () => {
+
+  it("should return the correct count of each item in a list as specified in the object", () => {
+
+    const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"];
+    const namesToCount = { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false };
+
+    assert.deepEqual(countOnly(firstNames, namesToCount), { Fang: 2, Jason: 1 });
+  });
+});
