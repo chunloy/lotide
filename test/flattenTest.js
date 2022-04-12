5de33dd4e7988ac36f77a35b6eb7e62816a6b865
Aaron Au <aaron.au@me.com>
2022-04-12 04:50:12 +0000

    Remove assertArraysEqual import.

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -1,10 +1,10 @@
 //import modules
-const { assertArraysEqual, flatten } = require('../index');
+const { flatten } = require('../index');
 const assert = require('chai').assert;
 
 describe("#flatten", () => {
   it("should return a flattened array for an array with a single level of nesting", () => {
-    assert.deepEqual(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6]);
+    assert.deepEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
   });
 
 });
