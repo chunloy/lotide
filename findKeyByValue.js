9fb498b0634d5f590b1f1ab13a4a82d50e74ff9b
Aaron Au <aaron.au@me.com>
2022-04-12 06:02:14 +0000

    Refactor code by moving test code to separate file.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -8,34 +8,4 @@ const findKeyByValue = function(object, value) {
 
   return undefined;
 };
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const bestTVShowsByGenre = {
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire",
-  crime: "Power",
-  reality: "American Ninja Warrior",
-  news: "CTV",
-  currentEvents: "CTV"
-};
-
-//test cases:
-const result = findKeyByValue(bestTVShowsByGenre, 'The Expanse'); 
-assertEqual(result, 'sciFi'); //passed, OK
-
-const result1 = findKeyByValue(bestTVShowsByGenre, 'American Ninja Warrior');
-assertEqual(result1, 'reality'); //passed, OK
-
-const result2 = findKeyByValue(bestTVShowsByGenre, 'South Park');
-assertEqual(result2, undefined); //passed, OK
-
-const result3 = findKeyByValue(bestTVShowsByGenre, 'CTV');
-assertEqual(result3, 'news'); //passed, OK
+module.exports = findKeyByValue;
