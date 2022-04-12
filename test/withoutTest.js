6f7923c94accb06096b2a9ca3d25b5fe2e3f8a37
Aaron Au <aaron.au@me.com>
2022-04-12 16:02:08 +0000

    Initial commit. Set up test code for M & C. Confirmed that tests run as expected.

---
+++ test/withoutTest.js
@@ -0,0 +1,18 @@
+//import modules
+const { without } = require('../index');
+const assert = require('chai').assert;
+
+describe("#without", () => {
+
+  it("should return [1, 2, 3] when passed [1, 'a', 2, 'b', 3, 'c']", () => {
+    assert.deepEqual(without([1, 'a', 2, 'b', 3, 'c'], ['a', 'b', 'c', 'd', 'e']), [1, 2, 3]);
+  });
+
+  it("should not modify the original array", () => {
+    const words = ["hello", "world", "lighthouse"];
+    without(words, ["lighthouse"]);
+    assert.deepEqual(words, ["hello", "world", "lighthouse"]);
+
+  });
+  
+});
