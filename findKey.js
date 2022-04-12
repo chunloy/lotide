840be7f39d00d88177fd5d7b66528277c34dc9b2
Aaron Au <aaron.au@me.com>
2022-04-12 05:48:34 +0000

    Refactor code by moving test code to separate file.

--- findKey.js
+++ findKey.js
@@ -8,35 +8,5 @@ const findKey = function(object, callback) {
   return undefined;
 };
 
-//TEST FUNCTION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const starsList = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri": { stars: 3 },
-  "noma": { stars: 2 },
-  "elBulli": { stars: 3 },
-  "Ora": { stars: 2 },
-  "Akelarre": { stars: 3 }
-};
-
-const fastFood = {
-  "McDonalds": { stars: 1 },
-  "Burger King": { stars: 2 },
-  "Wendy's": { stars: 2 },
-  "A&W": { stars: 2 },
-  "Subway": { stars: 2 },
-  "Pita Pit": { stars: 5 }
-};
-//test cases:
-assertEqual(findKey(starsList, x => x.stars === 2), 'noma'); //passed, OK 
-assertEqual(findKey(starsList, x => x.stars > 0), 'Blue Hill'); //passed, OK
-assertEqual(findKey(fastFood, x => x.stars < 5), 'McDonalds'); //passed, OK
-assertEqual(findKey(fastFood, x => x.stars > 5), undefined); //passed, OK
-
+//export modules
+module.exports = findKey;
