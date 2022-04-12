057ab47f3ce028de6625cf9acc37ccbd44da13b0
Aaron Au <aaron.au@me.com>
2022-04-12 05:14:18 +0000

    Refactor code by moving test code to separate file.

--- countOnly.js
+++ countOnly.js
@@ -1,15 +1,3 @@
-//TEST FUNCTION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//ACTUAL FUNCTION
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {}; //empty object to store count by name
 
@@ -28,21 +16,5 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+//export module
+module.exports = countOnly;
