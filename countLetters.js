230edf068916d2dc7f5eb909e6436ce5e217da29
Aaron Au <aaron.au@me.com>
2022-04-12 15:22:36 +0000

    Refactor code by moving test code to separate file.

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countLetters = function(string) {
   const result = {}; //empty array to store letter count
 
@@ -24,10 +16,5 @@ const countLetters = function(string) {
   return result;
 };
 
-//test cases:
-const result1 = countLetters('hello my name is aaron');
-
-assertEqual(result1['h'], 1); //passed, OK
-assertEqual(result1['l'], 2); //passed, OK
-assertEqual(result1['a'], 3); //passed, OK
-assertEqual(result1[' '], undefined); //passed OK
+//export module
+module.exports = countLetters;
