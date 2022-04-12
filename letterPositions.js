79d6320b0ba13e7f20eeb1b501e200cf3989c293
Aaron Au <aaron.au@me.com>
2022-04-12 19:39:26 +0000

    Remove code unnecessary code.

--- letterPositions.js
+++ letterPositions.js
@@ -1,23 +1,3 @@
-
-// const assertArraysEqual = function(arrayOne, arrayTwo) {
-//   console.log((eqArrays(arrayOne, arrayTwo) ?
-//     `🟢 Assertion Passed: [${arrayOne}] === [${arrayTwo}]` : `🔴 Assertion Failed: [${arrayOne}] !== [${arrayTwo}]`));
-// };
-
-// const eqArrays = function(arrayOne, arrayTwo) {
-//   //check if lengths match
-//   if (arrayOne.length !== arrayTwo.length) {
-//     return false;
-//   }
-//   //check if elements match
-//   for (let i = 0; i < arrayOne.length; i++) {
-//     if (arrayOne[i] !== arrayTwo[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
 const letterPositions = function(sentence) {
   const results = {}; //empty obhect to store an indices of each letter
 
@@ -38,17 +18,3 @@ const letterPositions = function(sentence) {
 
 //export module
 module.exports = letterPositions;
-
-// //test cases:
-// const results1 = letterPositions('hello');
-// console.log(results1);
-// assertArraysEqual(results1['h'], [0]);
-// assertArraysEqual(results1['e'], [1]);
-// assertArraysEqual(results1['l'], [2, 3]);
-// assertArraysEqual(results1['o'], [4]);
-
-// const results2 = letterPositions('hello my name is aaron');
-// assertArraysEqual(results2['m'], [6, 11]);
-// assertArraysEqual(results2['i'], [14]);
-
-
