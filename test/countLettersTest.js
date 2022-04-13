3a4ad4229ba56416977dfaabd2610729629f1f25
Aaron Au <aaron.au@me.com>
2022-04-13 04:40:48 +0000

    Fix reference issue when requiring countLetters.js

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,5 +1,5 @@
 //import modules
-const { countLetters } = require('../index');
+const  countLetters  = require('../countLetters');
 const assert = require('chai').assert;
 
 describe("#countLetters", () => {
